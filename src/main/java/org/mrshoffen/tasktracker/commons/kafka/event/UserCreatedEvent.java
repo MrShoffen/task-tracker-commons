68857698ab63b8038fb6016cac3891b84422e4af
andrey_vasiliev <mrshoffen@gmail.com>
2025-04-22 03:11:38 +0300

    initial commit

--- src/main/java/org/mrshoffen/tasktracker/commons/kafka/event/UserCreatedEvent.java
+++ src/main/java/org/mrshoffen/tasktracker/commons/kafka/event/UserCreatedEvent.java
@@ -1,13 +1,17 @@
 package org.mrshoffen.tasktracker.commons.kafka.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UserCreatedEvent {
-    UUID id;
-    String email;
+    private UUID id;
+    private String email;
 }
