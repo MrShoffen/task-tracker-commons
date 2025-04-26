068f5b2fdff2361fb6952c6cf98191e63de898ae
andrey_vasiliev <mrshoffen@gmail.com>
2025-04-26 23:23:45 +0300

    feat: add class for User Event

--- src/main/java/org/mrshoffen/tasktracker/commons/kafka/event/TopicsConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.mrshoffen.tasktracker.commons.kafka.event;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class TopicsConfiguration {
-
-    public static final String USER_CREATED_TOPIC_NAME = "event.users.created";
-}
--- src/main/java/org/mrshoffen/tasktracker/commons/kafka/event/user/UserCreatedEvent.java
+++ src/main/java/org/mrshoffen/tasktracker/commons/kafka/event/user/UserCreatedEvent.java
@@ -1,4 +1,4 @@
-package org.mrshoffen.tasktracker.commons.kafka.event;
+package org.mrshoffen.tasktracker.commons.kafka.event.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +12,10 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class UserCreatedEvent {
+
+    public static final String TOPIC = "event.users.created";
+
     private UUID id;
     private String email;
+    private String userIpAddr;
 }
