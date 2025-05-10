11a7532b14c8839223633b95da13eb20b6f018b2
andrey_vasiliev <mrshoffen@gmail.com>
2025-05-11 02:53:24 +0300

    feat: add user permission dto

---
+++ src/main/java/org/mrshoffen/tasktracker/commons/web/dto/UserPermissionDto.java
@@ -0,0 +1,15 @@
+package org.mrshoffen.tasktracker.commons.web.dto;
+
+import lombok.Data;
+import org.mrshoffen.tasktracker.commons.web.permissions.Permission;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class UserPermissionDto extends ResourceDto{
+    private Long id;
+    private UUID userId;
+    private UUID workspaceId;
+    private List<Permission> permissions;
+}
