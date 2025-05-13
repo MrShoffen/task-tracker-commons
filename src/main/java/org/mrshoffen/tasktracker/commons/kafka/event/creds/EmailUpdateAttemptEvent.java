d566ad4b5cc8c42da80304a4d21a5fff9fc2a73a
andrey_vasiliev <mrshoffen@gmail.com>
2025-05-13 20:50:45 +0300

    feat: add event for email change

--- src/main/java/org/mrshoffen/tasktracker/commons/kafka/event/creds/EmailUpdateAttemptEvent.java
+++ src/main/java/org/mrshoffen/tasktracker/commons/kafka/event/creds/EmailUpdateAttemptEvent.java
@@ -2,11 +2,13 @@ package org.mrshoffen.tasktracker.commons.kafka.event.creds;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmailUpdateAttemptEvent {
 
     public static final String TOPIC = "event.credentials.password-updated";
--- src/main/java/org/mrshoffen/tasktracker/commons/kafka/event/creds/EmailUpdatedSuccessEvent.java
+++ src/main/java/org/mrshoffen/tasktracker/commons/kafka/event/creds/EmailUpdatedSuccessEvent.java
@@ -2,11 +2,13 @@ package org.mrshoffen.tasktracker.commons.kafka.event.creds;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmailUpdatedSuccessEvent {
 
     public static final String TOPIC = "event.credentials.email-updated";
