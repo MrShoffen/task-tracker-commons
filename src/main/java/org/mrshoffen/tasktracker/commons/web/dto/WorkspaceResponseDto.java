21951a6d11fbb50f5106c608212e19909133ae61
mrshoffen <mrshoffen@gmail.com>
2025-05-22 16:25:59 +0300

    feat: add cover url to workspace response

--- src/main/java/org/mrshoffen/tasktracker/commons/web/dto/WorkspaceResponseDto.java
+++ src/main/java/org/mrshoffen/tasktracker/commons/web/dto/WorkspaceResponseDto.java
@@ -14,6 +14,7 @@ public class WorkspaceResponseDto extends ResourceDto {
     private UUID id;
     private String name;
     private Boolean isPublic;
+    private String coverUrl;
     private Instant createdAt;
 
     private UUID userId;
