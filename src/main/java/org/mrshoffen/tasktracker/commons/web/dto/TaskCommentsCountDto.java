96aa195ed2b10cee5fec68e0e993b5054c4fad70
andrey_vasiliev <mrshoffen@gmail.com>
2025-05-29 01:13:21 +0300

    feat: add count dto for message

---
+++ src/main/java/org/mrshoffen/tasktracker/commons/web/dto/TaskCommentsCountDto.java
@@ -0,0 +1,17 @@
+package org.mrshoffen.tasktracker.commons.web.dto;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TaskCommentsCountDto {
+    private UUID taskId;
+    private Long count;
+}
